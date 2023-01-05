e35da43aad2668cc29fb52c4d7dfd8602b179a77
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-01-05 01:09:57 +0000

    Deploying to gh-pages from @ Yurii-Kavuza/goit-react-hw-05-movies@ee77d133d3084e804c0f21c98458dcd7bbc226c3 🚀

---
+++ static/js/273.47c5060f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[273],{2273:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(4476),o=n(1087),s=n(184),u=function(){return(0,s.jsxs)(r.Z,{display:"flex",alignItems:"center",justifyContent:"cemter",p:3,children:["Sorry, we could not help you. Please return to the\xa0",(0,s.jsx)(o.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=273.47c5060f.chunk.js.map
